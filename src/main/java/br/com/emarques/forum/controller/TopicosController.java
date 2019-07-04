a263c086037ee9e1ac15b746d8353572b8f26086
Ewerton Marques <marquescpv@gmail.com>
2019-07-04 00:50:36 -0300

    Adicionado o Spring Data - Configurada a base de Dados

---
+++ src/main/java/br/com/emarques/forum/controller/TopicosController.java
@@ -0,0 +1,23 @@
+package br.com.emarques.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.emarques.forum.controller.dto.TopicoDto;
+import br.com.emarques.forum.model.Curso;
+import br.com.emarques.forum.model.Topico;
+
+@RestController
+public class TopicosController {
+	
+	@RequestMapping("/topicos")
+	public List<TopicoDto> lista(){
+		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
+		return TopicoDto.converter(Arrays.asList(topico,topico,topico));
+	}
+
+}
